223f7275baada3ea5ee35bb5af31c4436bed5196
Znoywho <znoywho1508@gmail.com>
2026-08-16 23:11:07 +0700

    Connection Management

--- Code/Server/app/network/client.py
+++ Code/Server/app/network/client.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 from websockets.client import connect
 
@@ -6,12 +7,20 @@ async def hello():
     uri = "ws://localhost:8765"
 
     async with connect(uri) as websocket:
-        message = "Hello, Websocket Server!"
-        print(f"Sending to sever: {message}")
-        await websocket.send(message)
+        while True:
+            message = str(input("Enter your message: \n/exit: out session\n/move: check in caro\n"))
+            message = message.strip()
+            if message == "/exit":
+                print("Connection from server is stopped!!")
+                break
+            if message == "/move":
+                x, y = input("(X, Y)").split()
+                movement = {"player": websocket.remote_address, "x": x, "y": y}
+                message = json.dumps(movement)
+            await websocket.send(message)
 
-        response = await websocket.recv()
-        print(f"Received from server: {response}")
+            response = await websocket.recv()
+            print(f"Received from server: {response}")
 
 
 if __name__ == "__main__":
--- Code/Server/app/network/server.py
+++ Code/Server/app/network/server.py
@@ -1,34 +1,50 @@
+import random
 import asyncio
+import json
 from websockets.server import serve
-from app.models.match import Role, match
+from models.match import Role, match
 
 
-class server:
+class handler:
     def __init__(self, addr, port):
         self.HOST = addr
         self.PORT = port
+        self.connected_users = set()
+        self.matches = {}
 
     def create_new_match(self, player1, player2):
         # NOTE: random ID
-        self.current_match = match(player1, player2, 100)
-        print(f"Create new match!: ID {self.current_match.match_id}")
+        new_match = match(player1, player2, random.random())
+        self.matches[new_match.match_id] = new_match
+        print(f"Create new match!: ID {new_match.match_id}")
 
-    async def server(self, addr, s):
+    # def match_state(self, match_id):
+    #     if match_id
+
+    async def ServerAction(self, websocket):
+        self.connected_users.add(websocket)
+        print(f"Number of clients are active: {len(self.connected_users)}")
         try:
-            print(f"Connected by: {addr}")
-            async for message in s:
-                print(f"Received raw data:\n{message}\nfrom {addr}")
+            print(f"Connected by: {websocket.remote_address}")
+            async for message in websocket:
+                print(f"Received raw data:\n{json.loads(message)}\nfrom {websocket.remote_address}")
+                await websocket.send("Server Already received message from you")
         except Exception as e:
             print(f"Error handling client: {e}")
+        finally:
+            # unregister client
+            self.connected_users.remove(websocket)
+            print(f"Client disconnected. Remaining clients {len(self.connected_users)}")
 
     async def serverAction(self):
-        async with serve(self.server, self.HOST, self.PORT) as ser:
+        async with serve(self.ServerAction, self.HOST, self.PORT) as ser:
             print(f"Websocket server is running on ws://{self.HOST}:{self.PORT}")
-            await ser.serve_forever()
+            # unsafe thread. await new session of connection in the future
+            await asyncio.Future()
 
 
 if __name__ == "__main__":
     HOST = "localhost"
-    PORT = "8765"
-    MyServer = server(HOST, PORT)
+    PORT = 8765
+    MyServer = handler(HOST, PORT)
     asyncio.run(MyServer.serverAction())
