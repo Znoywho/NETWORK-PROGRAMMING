3117f947885a9c4e4c24bb3de6544c1c8c4beffb
Znoywho <znoywho1508@gmail.com>
2026-08-25 08:35:21 +0700

    Database connection

--- Code/Server/app/db.py
+++ Code/Server/app/db.py
@@ -16,8 +16,31 @@ engine = create_engine(
         )
 print("run session local")
 
-SessionLocal = scoped_session(sessionmaker(bind=engine))
+SessionLocal = scoped_session(sessionmaker(bind=engine))# Create Thread safety 
+
+session = SessionLocal # Create new session (thread)
 
 def init_db():
     print("initialize database")
-    BASE.metadata.create_all(bind = engine) 
+    BASE.metadata.create_all(bind=engine)  # Denpendency tracking: create `table` in the exact model
+
+# EXAMPLE:
+#     from sqlalchemy import create_engine
+#     from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+#
+# # 1. Define the declarative base class
+#     class Base(DeclarativeBase):
+#         pass
+#
+# # 2. Define your ORM models inheriting from the Base
+#     class User(Base):
+#         __tablename__ = "users"
+#
+#         id: Mapped[int] = mapped_column(primary_key=True)
+#         name: Mapped[str]
+#
+# # 3. Create a database connection engine
+#     engine = create_engine("sqlite:///example.db")
+#
+# # 4. Generate all tables in the database
+#     Base.metadata.create_all(engine)
